8309aca5323752e1ccacc0cc6d61bee6ec4d838a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-04-02 14:30:13 +1300

    added comments and made the print look nicer

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
+# docstring - Luke Thompson - airplane database application
+# imports
 import sqlite3
 
-db = sqlite3.connect("fighters.db")
-cursor = db.cursor()
-sql = "SELECT * FROM fighter;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+# constants and variables
+DATABASE = "fighters.db"
+
+# functions
+def print_all_aircraft():
+    '''print all the aircraft nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # loop through all the results
+    print(f"name                          speed   max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    # loop finished here
+    db.close()    
+
+
+
+
+
+# main code
+print_all_aircraft()
