1684171e5763039ba303106e8c2fec28f4d0799c
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-04-02 14:15:11 +1300

    tested database

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import sqlite3
 
 db = sqlite3.connect("fighters.db")
 cursor = db.cursor()
-sql = "SELECT * FROM fighters;"
+sql = "SELECT * FROM fighter;"
 cursor.execute(sql)
 results = cursor.fetchall()
 print(results)
-
 db.close()
