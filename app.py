cfa928e8edada0e81cf2caf69ed8f310eee4c4b8
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-04-03 12:21:28 +1300

    added main menu with all queries

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def print_all_aircraft():
     cursor.execute(sql)
     results = cursor.fetchall()
     # loop through all the results
-    print(f"name                          speed   max_g climb range payload")
+    print("name                          speed   max_g climb range payload")
     for fighter in results:
         print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
     # loop finished here
@@ -25,4 +25,21 @@ def print_all_aircraft():
 
 
 # main code
-print_all_aircraft()
+while True:
+    user_input = input("\nWhat would you like to do. \n1. Print all aircraft \n2. Print all aircraft by speed\n3. Print all aircraft by max g force\n4. Print all aircraft by climbrate\n5. Print all aircraft by range\n6. Print all aircraft by payload\n7. Exit\n")
+    if user_input == "1":
+        print_all_aircraft()
+    elif user_input == "2":
+        print_all_aircraft_by_speed()
+    elif user_input == "3":
+        print_all_aircraft_by_g()
+    elif user_input == "4":
+        print_all_aircraft_by_climb()
+    elif user_input == "5":
+        print_all_aircraft_by_range()
+    elif user_input == "6":
+        print_all_aircraft_by_payload()
+    elif user_input == "7":
+        break 
+    else:
+        print("That was not an option\n")
