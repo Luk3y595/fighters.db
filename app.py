453e60205973ebd6321e9e78cab0a9c435db696b
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-04-02 14:06:43 +1300

    added basic python to db

---
+++ app.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+db = sqlite3.connect("fighters.db")
+cursor = db.cursor()
+sql = "SELECT * FROM fighters;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+
+db.close()
